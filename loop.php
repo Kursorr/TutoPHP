41a5c9605025b5cc33ef0d11f0f81c5e9a54ea0e
Ravaniss <senechal.max06@gmail.com>
2019-07-16 15:38:03 +0200

    Loop

---
+++ loop.php
@@ -0,0 +1,18 @@
+<?php
+  $i = 1;
+  while ($i <= 10) {
+    echo $i = $i + 1;
+  }
+
+  for ($i = 1; $i <= 10; $i++) {
+    echo $i;
+  }
+
+  foreach ($tableau as $valeur){
+    // du code
+  }
+
+  foreach ($tableau as $clef => $valeur){
+    // du code
+  }
+?>
